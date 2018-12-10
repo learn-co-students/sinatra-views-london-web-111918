3dfe509a3d6ad8edb36edd80eb8cd13ee64c3128
github username <github email address>
2018-12-10 14:37:57 +0000

    Done.

---
+++ views/run.rb
@@ -0,0 +1,11 @@
+
+require 'pry'
+require 'terminal-table'
+require_relative '../config/environment'
+require_relative "../lib/command_line_interface.rb"
+
+
+USERNAME = welcome
+username_search
+loop do run_main_menu
+end
